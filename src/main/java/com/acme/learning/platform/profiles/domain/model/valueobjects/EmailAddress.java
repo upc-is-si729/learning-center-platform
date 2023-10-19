2ecd09a55500458a1ae589773e80de608ea2c151
Angel Velasquez <pcisavel@upc.edu.pe>
2023-10-19 10:58:56 -0500

    feat(profiles): named email address as email.

--- src/main/java/com/acme/learning/platform/profiles/domain/model/valueobjects/EmailAddress.java
+++ src/main/java/com/acme/learning/platform/profiles/domain/model/valueobjects/EmailAddress.java
@@ -1,10 +1,12 @@
 package com.acme.learning.platform.profiles.domain.model.valueobjects;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import jakarta.validation.constraints.Email;
 
 @Embeddable
 public record EmailAddress(
+        @Column(name = "email")
         @Email
         String address) {
     public EmailAddress() {
