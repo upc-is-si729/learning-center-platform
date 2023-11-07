0863facaa7302d41f44360715cc340893fb550f8
Angel Velasquez <pcisavel@upc.edu.pe>
2023-11-07 12:51:15 -0500

    docs: added documentation to application event handler.

--- src/main/java/com/acme/learning/platform/iam/application/internal/eventhandlers/ApplicationReadyEventHandler.java
+++ src/main/java/com/acme/learning/platform/iam/application/internal/eventhandlers/ApplicationReadyEventHandler.java
@@ -10,6 +10,10 @@ import org.springframework.stereotype.Service;
 
 import java.sql.Timestamp;
 
+/**
+ * ApplicationReadyEventHandler class
+ * This class is used to handle the ApplicationReadyEvent
+ */
 @Service
 public class ApplicationReadyEventHandler {
     private final RoleCommandService roleCommandService;
@@ -19,6 +23,11 @@ public class ApplicationReadyEventHandler {
         this.roleCommandService = roleCommandService;
     }
 
+    /**
+     * Handle the ApplicationReadyEvent
+     * This method is used to seed the roles
+     * @param event the ApplicationReadyEvent the event to handle
+     */
     @EventListener
     public void on(ApplicationReadyEvent event) {
         var name = event.getApplicationContext().getId();
