cbff9b0c4cc7c25dd1bdf3ce3d4a2ef8e5abcab9
Angel Velasquez <pcisavel@upc.edu.pe>
2023-11-12 23:05:12 -0500

    feat(roles): added get role by name query and query handler.

--- src/main/java/com/acme/learning/platform/iam/domain/services/RoleQueryService.java
+++ src/main/java/com/acme/learning/platform/iam/domain/services/RoleQueryService.java
@@ -2,9 +2,12 @@ package com.acme.learning.platform.iam.domain.services;
 
 import com.acme.learning.platform.iam.domain.model.entities.Role;
 import com.acme.learning.platform.iam.domain.model.queries.GetAllRolesQuery;
+import com.acme.learning.platform.iam.domain.model.queries.GetRoleByNameQuery;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface RoleQueryService {
     List<Role> handle(GetAllRolesQuery query);
+    Optional<Role> handle(GetRoleByNameQuery query);
 }
